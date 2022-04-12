afeb040e1df9bb7c248d7039ef6ecbbd33660766
Sridhar <sridhar.gayam@gmail.com>
2022-04-13 02:59:59 +0530

    second commit

--- contracts/Thoritos.sol
+++ contracts/Thoritos.sol
@@ -45,12 +45,32 @@ contract Thoritos is ERC20Interface{
 
     event Mint(address indexedto, uint tokens); 
     event Burn(address indexedto, uint tokens); 
+    event Locked(address indexed _of, bytes32 indexed _reason, uint256 _amount, uint256 _validity);
+    event Unlocked(address indexed _of, bytes32 indexed _reason, uint256 _amount);
+
+    string internal constant ALREADY_LOCKED = "Tokens already locked";
+    string internal constant NOT_LOCKED = "No tokens locked";
+    string internal constant AMOUNT_ZERO = "Amount can not be 0";
+
+
+    mapping(address => bytes32[]) public lockReason;
+    struct lockToken {
+        uint256 amount;
+        uint256 validity;
+        bool claimed;
+    }
+    mapping(address => mapping(bytes32 => lockToken)) public locked;
 
     
     
     constructor(){
         founder = msg.sender;
     }    
+
+    modifier onlyFounder() {
+        require(founder == msg.sender, "not owner");
+        _;
+    }
     
     function balanceOf(address tokenOwner) public view override returns (uint balance){
         return balances[tokenOwner];
@@ -117,8 +137,121 @@ contract Thoritos is ERC20Interface{
         return true;
     }
 
-    modifier onlyFounder() {
-        require(founder == msg.sender, "not owner");
-        _;
+    
+    function tokensLocked(address _of, bytes32 _reason) public view returns (uint256 amount)
+    {
+        if (!locked[_of][_reason].claimed)
+            amount = locked[_of][_reason].amount;
     }
+
+    function tokensLockedAtTime(address _of, bytes32 _reason, uint256 _time) public view returns (uint256 amount)
+    {
+        if (locked[_of][_reason].validity > _time)
+            amount = locked[_of][_reason].amount;
+    }
+
+    function totalBalanceOf(address _of) public view returns (uint256 amount)
+    {
+        amount = balanceOf(_of);
+
+        for (uint256 i = 0; i < lockReason[_of].length; i++) {
+            amount += tokensLocked(_of, lockReason[_of][i]);
+        }   
+    }
+
+    function lock(bytes32 _reason, uint256 _amount, uint256 _time) public returns (bool)
+    {
+        require(tokensLocked(msg.sender, _reason) == 0, ALREADY_LOCKED);
+        require(_amount != 0, AMOUNT_ZERO);
+        uint256 validUntil = block.timestamp + _time; 
+        if (locked[msg.sender][_reason].amount == 0)
+            lockReason[msg.sender].push(_reason);
+
+        transfer(address(this), _amount);
+
+        locked[msg.sender][_reason] = lockToken(_amount, validUntil, false);
+
+        emit Locked(msg.sender, _reason, _amount, validUntil);
+        return true;
+    }
+
+    function transferWithLock(address _to, bytes32 _reason, uint256 _amount, uint256 _time) public returns (bool)
+    {
+        uint256 validUntil = block.timestamp + _time;
+        require(tokensLocked(_to, _reason) == 0, ALREADY_LOCKED);
+        require(_amount != 0, AMOUNT_ZERO);
+
+        if (locked[_to][_reason].amount == 0)
+            lockReason[_to].push(_reason);
+
+        transfer(address(this), _amount);
+
+        locked[_to][_reason] = lockToken(_amount, validUntil, false);
+        
+        emit Locked(_to, _reason, _amount, validUntil);
+        return true;
+    }
+
+    function extendLock(bytes32 _reason, uint256 _time) public returns (bool)
+    {
+        require(tokensLocked(msg.sender, _reason) > 0, NOT_LOCKED);
+
+        locked[msg.sender][_reason].validity = locked[msg.sender][_reason].validity + _time;
+
+        emit Locked(msg.sender, _reason, locked[msg.sender][_reason].amount, locked[msg.sender][_reason].validity);
+        return true;
+    }
+
+    function increaseLockAmount(bytes32 _reason, uint256 _amount) public returns (bool)
+    {
+        require(tokensLocked(msg.sender, _reason) > 0, NOT_LOCKED);
+        transfer(address(this), _amount);
+
+        locked[msg.sender][_reason].amount += _amount;
+
+        emit Locked(msg.sender, _reason, locked[msg.sender][_reason].amount, locked[msg.sender][_reason].validity);
+        return true;
+    }
+
+    function tokensUnlockable(address _of, bytes32 _reason) public view returns (uint256 amount)
+    {
+        if (locked[_of][_reason].validity >= block.timestamp && !locked[_of][_reason].claimed)
+                   amount = locked[_of][_reason].amount;
+    }
+
+    function unlock(address _of) public onlyFounder returns (uint256 unlockableTokens)
+    {
+        uint256 lockedTokens;
+
+        for (uint256 i = 0; i < lockReason[_of].length; i++) {
+            lockedTokens = tokensUnlockable(_of, lockReason[_of][i]);
+            if (lockedTokens > 0) {
+                unlockableTokens += lockedTokens;
+                locked[_of][lockReason[_of][i]].claimed = true;
+                emit Unlocked(_of, lockReason[_of][i], lockedTokens);
+            }
+        } 
+
+        if (unlockableTokens > 0)
+            this.transfer(_of, unlockableTokens);
+    }
+
+    function getUnlockableTokens(address _of) public view returns (uint256 unlockableTokens)
+    {
+        for (uint256 i = 0; i < lockReason[_of].length; i++) {
+            unlockableTokens += tokensUnlockable(_of, lockReason[_of][i]);
+        }          
+    }
+
+    function currentTimeStamp() view public returns (uint256 currentTime)
+    {
+        currentTime = block.timestamp;
+    }
+
+    function validUntilOf(address _of, bytes32 _reason ) view public returns (uint256 time)
+    {
+        time = locked[_of][_reason].validity;
+    }
+
+
 }
--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,8 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Eth }  from "web3-eth";
+//import { Web3 }  from "web3";
+import web3 from "web3"
 
 describe("RevInfotech-Thoritos", function () {
   let Admin;
@@ -8,6 +11,11 @@ describe("RevInfotech-Thoritos", function () {
   let UserThree: any;
   let Thoritos;
   let thoritos: any;
+  const lockReason = '0x4c4f434b31000000000000000000000000000000000000000000000000000000';
+  const lockReason2 = '0x4c4f434b32000000000000000000000000000000000000000000000000000000';
+  const lockReason3 = '0x4c4f434b33000000000000000000000000000000000000000000000000000000';
+  const lockedAmount = 200;
+  const lockPeriod = 499;
 
       beforeEach(async function () {
         [Admin,UserOne,UserTwo,UserThree] = await ethers.getSigners();
@@ -81,4 +89,67 @@ describe("RevInfotech-Thoritos", function () {
         ).to.be.revertedWith("not owner");        
       });
 
+      it('Has the right total balance for the contract owner', async () => {
+        let supply = await thoritos.totalSupply();
+        console.log("intial supply after contract deployed : ", supply.toNumber());
+        await thoritos.mint(UserOne.address, 1000);
+        supply = await thoritos.totalSupply();  
+        
+        const userOneBal = await thoritos.totalBalanceOf(UserOne.address);
+        console.log("totalBalanceOf userOne after minting : ", userOneBal.toNumber());
+        expect(userOneBal).to.equal(1000);    
+      });
+      it('locked tokens from transferable balance', async () => {
+        await thoritos.mint(UserOne.address, 1000); 
+        let userOneTotalBal = await thoritos.totalBalanceOf(UserOne.address);
+        console.log("totalBalanceOf userOne after minting : ", userOneTotalBal.toNumber());
+        expect(userOneTotalBal).to.equal(1000);
+
+        await thoritos.connect(UserOne).lock(lockReason, lockedAmount, lockPeriod);
+        let userOneBal = await thoritos.balanceOf(UserOne.address);
+        console.log("balanceOf userOne after locking 200 tokens : ", userOneBal.toNumber());
+        expect(userOneBal).to.equal(userOneTotalBal - lockedAmount);
+
+        let contractBal = await thoritos.balanceOf(thoritos.address);
+        console.log("balanceOf contract after locking 200 tokens : ", contractBal.toNumber());
+        expect(contractBal).to.equal(lockedAmount);
+
+        let userOneUnlockBal =  await thoritos.getUnlockableTokens(UserOne.address);
+        console.log("unlock balanceOf user after locking 200 tokens : ", userOneUnlockBal.toNumber());
+        expect(userOneUnlockBal).to.equal(lockedAmount);
+
+        // let currentTimeStamp =  await thoritos.currentTimeStamp();
+        // console.log("currentTimeStamp : ", currentTimeStamp.toNumber());
+        // let validUntilOf =  await thoritos.validUntilOf(UserOne.address,lockReason);
+        // console.log("validUntilOf userOne : ", validUntilOf.toNumber());
+      });
+
+      it('reverts locking more tokens via lock function', async () => {
+        await thoritos.mint(UserOne.address, 1000); 
+        let userOneTotalBal = await thoritos.totalBalanceOf(UserOne.address); 
+        await expect(
+          thoritos.connect(UserOne).lock(lockReason, userOneTotalBal+1, lockPeriod)
+        ).to.be.revertedWith("not enoguh tokens"); 
+      });
+
+      it('can increase the number of tokens locked', async () => {
+        await thoritos.mint(UserOne.address, 1000); 
+        await thoritos.connect(UserOne).lock(lockReason, lockedAmount, lockPeriod);     
+        let userOneUnlockBal =  await thoritos.getUnlockableTokens(UserOne.address);
+        console.log("userOneUnlockBal after locking 200 tokens : ", userOneUnlockBal.toNumber());
+        await thoritos.connect(UserOne).increaseLockAmount(lockReason, lockedAmount);
+        userOneUnlockBal =  await thoritos.getUnlockableTokens(UserOne.address);
+        console.log("userOneUnlockBal after locking 200 more tokens : ", userOneUnlockBal.toNumber());
+        expect(userOneUnlockBal).to.equal(2*lockedAmount);
+      });
+
+      it('can unLock tokens', async () => {
+        await thoritos.mint(UserOne.address, 1000); 
+        await thoritos.connect(UserOne).lock(lockReason, lockedAmount, lockPeriod);
+        let userOneUnlockBal =  await thoritos.getUnlockableTokens(UserOne.address);
+        expect(userOneUnlockBal).to.equal(lockedAmount); 
+        await thoritos.unlock(UserOne.address)
+        let userOneBal = await thoritos.balanceOf(UserOne.address);
+        expect(userOneBal).to.equal(1000);
+      });
 });
